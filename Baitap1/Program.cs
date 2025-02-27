3c94ca4ad6d4a6e1a1ad9c6f1a9ca3d71597a358
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-27 22:19:06 +0700

    Hoan thanh bai 1

--- Baitap1/Program.cs
+++ Baitap1/Program.cs
@@ -7,15 +7,15 @@
             public string giongCho { get; set; }
             public string mauLong { get; set; }
 
-            public Dog(string giongCho, string mauLong)
+            public Dog(string giongCho_231, string mauLong_231)
             {
-                this.giongCho = giongCho;
-                this.mauLong = mauLong;
+                this.giongCho = giongCho_231;
+                this.mauLong = mauLong_231;
             }
 
-            public static Dog CreateDog(string giongCho, string mauLong = "trang")
+            public static Dog CreateDog(string giongCho_231, string mauLong_231 = "Trang")
             {
-                return new Dog(giongCho, mauLong);
+                return new Dog(giongCho_231, mauLong_231);
             }
 
             public void ShowInfo()
@@ -25,11 +25,11 @@
         }
         static void Main(string[] args)
         {
-            Dog dog1 = Dog.CreateDog("Husky", "den");
-            dog1.ShowInfo();
+            Dog dog1_231 = Dog.CreateDog("Husky", "den");
+            dog1_231.ShowInfo();
 
-            Dog dog2 = Dog.CreateDog("Poodle");
-            dog2.ShowInfo();
+            Dog dog2_231 = Dog.CreateDog("Poodle");
+            dog2_231.ShowInfo();
         }
     }
 }
--- Baitap1/obj/Debug/net8.0/Baitap1.AssemblyInfo.cs
+++ Baitap1/obj/Debug/net8.0/Baitap1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baitap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648bf3cf27e9f622c662713d172de02304ecfd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baitap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baitap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
