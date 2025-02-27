eb7718cb73bc1591f6ac15996a0127c7f0df0e5e
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-27 22:15:46 +0700

    Demo code bai 2

---
+++ Baitap2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    enum DogBreed
+    {
+        Labrador, Husky, Poodle, Bulldog, Beagle, GoldenRetriever, GermanShepherd
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Danh sach cac giong cho hien co trong quay");
+            foreach (DogBreed tmp in Enum.GetValues(typeof(DogBreed)))
+            {
+                Console.WriteLine(((int)tmp + 1) + "." + tmp);
+            }
+            Console.Write("\nNhap so ma giong cho ban quan tam : ");
+            if (int.TryParse(Console.ReadLine(), out int userChoice))
+            {
+                if (Enum.IsDefined(typeof(DogBreed), userChoice - 1))
+                {
+                    Console.WriteLine("Ban dang tim hieu ve : " + (DogBreed)(userChoice - 1));
+                }
+                else
+                {
+                    Console.WriteLine("Khong co giong cho nay!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vui long nhap so!");
+            }
+        }
+    }
+}
