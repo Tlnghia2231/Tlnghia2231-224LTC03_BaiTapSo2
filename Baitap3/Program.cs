420cc0fa88405434861c09f420a01edacfe42466
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-27 22:13:04 +0700

    Hoan thanh bai 3

--- Baitap3/Program.cs
+++ Baitap3/Program.cs
@@ -13,11 +13,11 @@ namespace Baitap3
         public DogBreed Breed { get; set; }
         public string FurColor { get; set; }
 
-        public Dog(string name, DogBreed breed, string furColor = "Trang")
+        public Dog(string name_231, DogBreed breed_231, string furColor_231 = "Trang")
         {
-            Name = name;
-            Breed = breed;
-            FurColor = furColor;
+            Name = name_231;
+            Breed = breed_231;
+            FurColor = furColor_231;
         }
 
         public void ShowInfo()
@@ -29,20 +29,20 @@ namespace Baitap3
     {
         static void Main(string[] args)
         {
-            Dog dog1 = new Dog("Max", DogBreed.Labrador, "Vang");
-            Dog dog2 = new Dog("Bella", DogBreed.Husky);
-            Dog dog3 = new Dog("Charlie", DogBreed.Poodle, "Nau");
-            Dog dog4 = new Dog("Rocky", DogBreed.Bulldog, "Dom trang");
-            Dog dog5 = new Dog("Daisy", DogBreed.Beagle);
-            Dog dog6 = new Dog("Lucky", DogBreed.GoldenRetriever, "Vang kem");
-            Dog dog7 = new Dog("Duke", DogBreed.GermanShepherd);
-            Dog dog8 = new Dog("Teddy", DogBreed.Poodle, "Trang");
-            Dog dog9 = new Dog("Luna", DogBreed.Husky, "Xam");
-            Dog dog10 = new Dog("Bruno", DogBreed.Labrador, "Den");
+            Dog dog1_231 = new Dog("Max", DogBreed.Labrador, "Vang");
+            Dog dog2_231 = new Dog("Bella", DogBreed.Husky);
+            Dog dog3_231 = new Dog("Charlie", DogBreed.Poodle, "Nau");
+            Dog dog4_231 = new Dog("Rocky", DogBreed.Bulldog, "Dom trang");
+            Dog dog5_231 = new Dog("Daisy", DogBreed.Beagle);
+            Dog dog6_231 = new Dog("Lucky", DogBreed.GoldenRetriever, "Vang kem");
+            Dog dog7_231 = new Dog("Duke", DogBreed.GermanShepherd);
+            Dog dog8_231 = new Dog("Teddy", DogBreed.Poodle, "Trang");
+            Dog dog9_231 = new Dog("Luna", DogBreed.Husky, "Xam");
+            Dog dog10_231 = new Dog("Bruno", DogBreed.Labrador, "Den");
 
-            List<Dog> dogs = new List<Dog> { dog1, dog2, dog3, dog4, dog5, dog6, dog7, dog8, dog9, dog10 };
+            List<Dog> dogs_231 = new List<Dog> { dog1_231, dog2_231, dog3_231, dog4_231, dog5_231, dog6_231, dog7_231, dog8_231, dog9_231, dog10_231 };
             Console.WriteLine("\nThong tin cac chu cho:");
-            foreach (Dog dog in dogs)
+            foreach (Dog dog in dogs_231)
             {
                 dog.ShowInfo();
             }
--- Baitap3/obj/Debug/net8.0/Baitap3.AssemblyInfo.cs
+++ Baitap3/obj/Debug/net8.0/Baitap3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baitap3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798c3457afa85c4dc45bbc28907666b2a70e4a52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484a65083045f98538a08188f52b4c2cda81e10a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baitap3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baitap3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
